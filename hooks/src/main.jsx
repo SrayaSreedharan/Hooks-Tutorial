bd2e932533ab889419f4dfb839c484dc698f2f59
SRAYA SREEDHARAN <srayasreedharan111@gmail.com>
2025-06-18 11:42:20 +0530

    push

--- hooks/src/main.jsx
+++ hooks/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Usestate from './pages/Usestate.jsx'
+import UseReducer from './pages/UseReducer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
-    <Usestate/>
+    {/* <Usestate/> */}
+    <UseReducer/>
   </StrictMode>,
 )
--- hooks/src/pages/UseReducer.jsx
+++ hooks/src/pages/UseReducer.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+// similar to usestate but Used for managing more complex state 
+// const [state, dispatch] = useReducer(reducer, initialState);
+//         reducer: A pure function that takes the current state nd returns the new state. 
+//         initialState: The initial value of the state.
+//         state:current value
+        
+import React, { useReducer } from 'react'
+const initialState={count:0}
+ function reducer(state, action) {
+      switch (action.type) {
+        case 'inc':
+          return { count: state.count + 1 };
+        case 'dec':
+          return { count: state.count - 1 };
+        default:
+          throw new Error();
+      }
+    }
 
 const UseReducer = () => {
+    const[state,dispatch]= useReducer(reducer, initialState);
   return (
     <div>
-      
+        <h1>{state.count}</h1>
+        <button onClick={() => dispatch({ type: 'inc' })}>Increment</button>
+        <button onClick={() => dispatch({ type: 'dec' })}>Decrement</button>
     </div>
   )
 }
-
 export default UseReducer
